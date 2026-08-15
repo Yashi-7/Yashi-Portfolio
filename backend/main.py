5c9ef66b70dd0c84e8399e85e50fe6f17666b25b
Yashi-7 <yadavyashi@gmail.com>
2026-08-15 12:54:22 +0530

    Again Fix deployed backend API URL

--- backend/main.py
+++ backend/main.py
@@ -41,8 +41,9 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-         "http://localhost:5173",
+        "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://yashi-portfolio.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
